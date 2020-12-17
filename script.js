08091ea05a575cc1dae57245661423056dc47aca
ccsxw6 <ccsxw6@gmail.com>
2020-12-16 20:40:26 -0600

    got ajax to work and amend to page

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 $(document).ready(function() {
 
-var cityName = "festus, missouri"
-var apiKey = "ed6e80c5dec753844794759a64cbcdee"
-var queryUrl = "api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
+  $("#search-button").click(function() {
+    userInput = $("#city-input").val() //global variable
+    console.log(userInput)
+    weatherApi()
+  })
+    
+    var weatherApi = function() {
+        var apiKey = "ed6e80c5dec753844794759a64cbcdee"
+        var queryUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&appid=" + apiKey;
 
-$.ajax({
-    url: queryUrl,
-    method: "GET"
-}).then(function(response) {
-    console.log(response)
-})
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response)
+            $("#city-name").html(response.city.name)
 
+            var today = new Date().toLocaleDateString()
+            $("#city-name").append("</br>" + today)
+        })
+    }
 })
 
 //not working mate
