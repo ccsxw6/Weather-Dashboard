9573df40800ba6828f124835a3c149b4bd338b38
ccsxw6 <ccsxw6@gmail.com>
2020-12-18 15:07:48 -0600

    might redo whole set up when I get home

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
 $(document).ready(function() {
-
-var cityName = "festus, missouri"
-var apiKey = "ed6e80c5dec753844794759a64cbcdee"
-var queryUrl = "api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
-
-$.ajax({
-    url: queryUrl,
-    method: "GET"
-}).then(function(response) {
-    console.log(response)
-})
-
+    
+    
+    
+    $("#search-button").on("click", function() {
+        cityName =  $(".form-control").val()
+        renderInput()
+    })
+    
+    
+    var renderInput = function() {
+        
+        var apiKey = "9c596b570aec8a175e86b3272dcaa420"
+        var queryUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey
+        
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response)
+            
+            $("#city-name").html(response.city.name)
+            
+            //current day
+            var today = new Date();
+            var dd = String(today.getDate()).padStart(2, '0');
+            var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+            var yyyy = today.getFullYear();
+            today = mm + '/' + dd + '/' + yyyy;
+            $("#dateDiv").html(today)
+            
+        })
+    }
+    
 })
 
 //not working mate
